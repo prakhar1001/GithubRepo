ae9d90246ad84617e22b8b8389a9756efeebb1ac
Prakhar <prakhar@aadriinfin.com>
2017-03-01 21:51:33 +0530

    Savedinstancestate

--- app/src/main/java/prakhar/com/githubrepo/MainActivity.java
+++ app/src/main/java/prakhar/com/githubrepo/MainActivity.java
@@ -33,52 +33,52 @@ public class MainActivity extends AppCompatActivity {
 
 
     //Dao --> Data Access Object
-    private RepoDao repoDao; // Sql access object
-    private Repo repo; // Used for creating a LOG Object
+    private RepoDao mRepoDao; // Sql access object
+    private Repo mRepo; // Used for creating a LOG Object
 
 
-    private final String DB_NAME = "repo-db";  //Name of Db file in the Device
+    private final String DB_NAME = "mRepo-db";  //Name of Db file in the Device
 
-    ListView repoListView;
+    ListView mRepoListView;
     private ProgressDialog mProgressDialog;
-    MaterialSearchView searchView;
-    RepoAdapter repoAdapter;
-    ArrayList<GithubRepo.Item> repoArrayList;
-    Toolbar toolbar;
-    String globalquery = "Github Repo";
-    boolean repoListStatus = false;
+    MaterialSearchView mSearchView;
+    RepoAdapter mRepoAdapter;
+    ArrayList<GithubRepo.Item> mRepoArrayList;
+    Toolbar mToolbar;
+    String mGlobalQuery = "Github Repo";
+    boolean mRepoListStatus = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        repoListView = (ListView) findViewById(R.id.repolist);
+        mRepoListView = (ListView) findViewById(R.id.repolist);
 
         mProgressDialog = displayProgressDialog(this);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
-        repoDao = setup_db();
+        mRepoDao = setup_db();
 
         // TODO: 2/28/2017 get arraylist of bookmarked repos from database with their html urls
-        repoAdapter = new RepoAdapter(MainActivity.this, changeRepoToGithubItem(getFromSQL()));
-        repoListView.setAdapter(repoAdapter);
+        mRepoAdapter = new RepoAdapter(MainActivity.this, changeRepoToGithubItem(getFromSQL()));
+        mRepoListView.setAdapter(mRepoAdapter);
 
-        repoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mRepoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (repoArrayList != null) {
-                    repo = new Repo(null, repoArrayList.get(position).getFullName(), repoArrayList.get(position).getHtmlUrl());
-                    saveObjectToSQL(repo);
+                if (mRepoArrayList != null) {
+                    mRepo = new Repo(null, mRepoArrayList.get(position).getFullName(), mRepoArrayList.get(position).getHtmlUrl());
+                    saveObjectToSQL(mRepo);
                 }
             }
         });
 
 
-        searchView = (MaterialSearchView) findViewById(R.id.search_view);
-        searchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+        mSearchView = (MaterialSearchView) findViewById(R.id.search_view);
+        mSearchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
+        mSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 //Do some magic
@@ -92,25 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-            @Override
-            public void onSearchViewShown() {
-                //Do some magic
-            }
-
-            @Override
-            public void onSearchViewClosed() {
-                //Do some magic
-            }
-        });
-
-        //if (NetworkUtils.isNetworkConnected(MainActivity.this)) {
-
-        /*}else {
-            dismissProgress();
-            Toast.makeText(MainActivity.this, "Please connect with working network.", Toast.LENGTH_LONG).show();
-        }*/
     }
 
     public ArrayList<GithubRepo.Item> changeRepoToGithubItem(List<Repo> repo) {
@@ -136,11 +117,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public List<Repo> getFromSQL() {
-        List<Repo> repo_List = repoDao.queryBuilder().orderDesc(RepoDao.Properties.Id).build().list();
+        List<Repo> repo_List = mRepoDao.queryBuilder().orderDesc(RepoDao.Properties.Id).build().list();
         //Get the list of all LOGS in Database in descending order
 
         if (repo_List.size() > 0) {  //if list is not null
-
             return repo_List;
             //get(0)--> 1st object
             // getText() is the function in LOG class
@@ -148,23 +128,21 @@ public class MainActivity extends AppCompatActivity {
         return repo_List;
     }
 
-
     private void callGithubRepoAPI(final String query) {
-
         showProgress();
         Call<GithubRepo> GithubRepoCall = RetroClient.getInstance().getRetrofit().create(APIInterface.class).GET_REPO_FULLNAME(query);
         GithubRepoCall.enqueue(new Callback<GithubRepo>() {
             @Override
             public void onResponse(Call<GithubRepo> call, Response<GithubRepo> response) {
                 dismissProgress();
-                toolbar.setTitle(query);
-                toolbar.setTitleTextColor(Color.WHITE);
-                globalquery = query;
-                repoListStatus = true;
-                repoArrayList = new ArrayList<GithubRepo.Item>();
-                repoArrayList = (ArrayList<GithubRepo.Item>) response.body().getItems();
-                if (repoArrayList.size() > 0) {
-                    repoAdapter.addData(repoArrayList);
+                mToolbar.setTitle(query);
+                mToolbar.setTitleTextColor(Color.WHITE);
+                mGlobalQuery = query;
+                mRepoListStatus = true;
+                mRepoArrayList = new ArrayList<GithubRepo.Item>();
+                mRepoArrayList = (ArrayList<GithubRepo.Item>) response.body().getItems();
+                if (mRepoArrayList.size() > 0) {
+                    mRepoAdapter.addData(mRepoArrayList);
                 } else
                     Toast.makeText(MainActivity.this, "No Related Repo Found", Toast.LENGTH_LONG).show();
             }
@@ -185,51 +163,43 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
 
         MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
+        mSearchView.setMenuItem(item);
 
         return true;
     }
 
-    @Override
-    public void onBackPressed() {
-        if (searchView.isSearchOpen()) {
-            searchView.closeSearch();
-        } else if (!searchView.isSearchOpen() && !repoListStatus) {
-            super.onBackPressed();
-        }
-        if (repoListStatus) {
-            repoListStatus = false;
-            repoAdapter = new RepoAdapter(MainActivity.this, changeRepoToGithubItem(getFromSQL()));
-            repoListView.setAdapter(repoAdapter);
-        }
-
-
-    }
 
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         System.out.println("TAG, onSavedInstanceState");
 
-        outState.putString("query", globalquery);
-        outState.putParcelableArrayList("ParceableRepoList", repoArrayList);
+        outState.putString("query", mGlobalQuery);
+        outState.putParcelableArrayList("ParceableRepoList", mRepoArrayList);
     }
 
     protected void onRestoreInstanceState(Bundle savedState) {
         System.out.println("TAG, onRestoreInstanceState");
 
-        repoArrayList = savedState.getParcelableArrayList("ParceableRepoList");
-        repoAdapter.addData(repoArrayList);
+        mRepoArrayList = savedState.getParcelableArrayList("ParceableRepoList");
+        mRepoAdapter.addData(mRepoArrayList);
 
-        globalquery = savedState.getString("query");
+        mGlobalQuery = savedState.getString("query");
 
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onBackPressed() {
+        if (mSearchView.isSearchOpen()) {
+            mSearchView.closeSearch();
+        } else if (!mSearchView.isSearchOpen() && !mRepoListStatus) {
+            super.onBackPressed();
+        }
+        if (mRepoListStatus) {
+            mRepoListStatus = false;
+            mRepoAdapter = new RepoAdapter(MainActivity.this, changeRepoToGithubItem(getFromSQL()));
+            mRepoListView.setAdapter(mRepoAdapter);
+        }
 
-        toolbar.setTitle(globalquery);
-        toolbar.setTitleTextColor(Color.WHITE);
 
     }
 
@@ -242,6 +212,16 @@ public class MainActivity extends AppCompatActivity {
         if (mProgressDialog != null && !mProgressDialog.isShowing())
             mProgressDialog.show();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mToolbar.setTitle(mGlobalQuery);
+        mToolbar.setTitleTextColor(Color.WHITE);
+
+    }
+
 
     @Override
     public void onDestroy() {
@@ -259,6 +239,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    public void saveObjectToSQL(Repo repo_object) {
+        mRepoDao.insert(repo_object);
+    }
+
+    //Return the Configured LogDao Object
+    public RepoDao setup_db() {
+        DaoMaster.DevOpenHelper masterHelper = new DaoMaster.DevOpenHelper(this, DB_NAME, null); //create database db file if not exist
+        SQLiteDatabase db = masterHelper.getWritableDatabase();  //get the created database db file
+        DaoMaster master = new DaoMaster(db);//create masterDao
+        DaoSession masterSession = master.newSession(); //Creates Session session
+        return masterSession.getRepoDao();
+    }
+
     public static ProgressDialog displayProgressDialog(Context context) {
         ProgressDialog progressDialog = ProgressDialog.show(context, "", "", true);
         progressDialog.setContentView(R.layout.tresbudialog);
@@ -269,20 +263,6 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         progressDialog.dismiss();
         return progressDialog;
-    }
-
-
-    public void saveObjectToSQL(Repo repo_object) {
-        repoDao.insert(repo_object);
-    }
-
-    //Return the Configured LogDao Object
-    public RepoDao setup_db() {
-        DaoMaster.DevOpenHelper masterHelper = new DaoMaster.DevOpenHelper(this, DB_NAME, null); //create database db file if not exist
-        SQLiteDatabase db = masterHelper.getWritableDatabase();  //get the created database db file
-        DaoMaster master = new DaoMaster(db);//create masterDao
-        DaoSession masterSession = master.newSession(); //Creates Session session
-        return masterSession.getRepoDao();
     }
 
 
